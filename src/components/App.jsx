6c7956790dae14bc09f7f815272eaaf79d4c372e
Puran Singh Rana <puransinghrana080@gmail.com>
2025-01-05 01:50:54 +0530

    first commit , first react app

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+import Header from "./Header"
+import Note from "./Note"
+import Footer from "./Footer"
+
+function App(){
+    return (<>
+<Header />
+<Note />
+<Footer />
+</>
+    )
+}
+export default App;
---
+++ src/components/Note.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+
+function Note(){
+    return <div>
+        <h1>This is Note Title</h1>
+        <p>This is Note content</p>
+    </div>
+}
+
+export default Note;
